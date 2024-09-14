f849231b7da1d64e12f352e2b74eac3b9ff3d3c6
Eamon Boyle <blaowskate@hotmail.com>
2024-09-14 12:56:49 +0100

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", async (req: Request, res: Response) => {
+    res.send("Hello World");
+});
+
+const PORT = process.env.PORT || 7000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+});
