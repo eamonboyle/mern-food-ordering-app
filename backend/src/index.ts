42836e20d68709d317cca721a11dbcc275f36406
Eamon Boyle <blaowskate@hotmail.com>
2024-09-14 15:01:39 +0100

    Connect to MongoDB database on startup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING!).then(() => console.log("Connected to database"));
+
 const app = express();
 app.use(express.json());
 app.use(cors());
