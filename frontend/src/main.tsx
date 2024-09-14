1d26017bfbb6bfd9c6712d9f1982397ff7a98f0f
Eamon Boyle <blaowskate@hotmail.com>
2024-09-14 17:15:25 +0100

    feat: Add @auth0/auth0-react dependency and implement login functionality

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./globals.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./app-routes";
+import Auth0ProviderWithNavigate from "./auth/auth0-provider-with-navigate";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Router>
-            <AppRoutes />
+            <Auth0ProviderWithNavigate>
+                <AppRoutes />
+            </Auth0ProviderWithNavigate>
         </Router>
     </StrictMode>
 );
