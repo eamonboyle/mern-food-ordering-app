7bd776803d615c551dca44e3ccd0106479c86a06
Eamon Boyle <blaowskate@hotmail.com>
2024-09-14 16:23:44 +0100

    feat: Add Hero component and Footer component
    - Add Hero component to display a hero image on the homepage
    - Add Footer component to display the footer section on all pages

--- frontend/src/app-routes.tsx
+++ frontend/src/app-routes.tsx
@@ -1,14 +1,27 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
+import HomePage from "./pages/home-page";
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<p>Home Page</p>} />
-                <Route path="user-profile" element={<p>User Profile Page</p>} />
-                <Route path="*" element={<Navigate to="/" />} />
-            </Route>
+            <Route
+                path="/"
+                element={
+                    <Layout showHero={true}>
+                        <HomePage />
+                    </Layout>
+                }
+            />
+            <Route
+                path="/user-profile"
+                element={
+                    <Layout>
+                        <p>User Profile Page</p>
+                    </Layout>
+                }
+            />
+            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     );
 };
--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,14 +1,22 @@
+import Footer from "@/components/footer";
 import Header from "@/components/header";
-import { Outlet } from "react-router-dom";
+import Hero from "@/components/hero";
 
-const Layout = () => {
+interface Props {
+    children: React.ReactNode;
+    showHero?: boolean;
+}
+
+const Layout = ({ children, showHero = false }: Props) => {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
 
-            <div className="container mx-auto flex-1 py-10">
-                <Outlet />
-            </div>
+            {showHero && <Hero />}
+
+            <div className="container mx-auto flex-1 py-10">{children}</div>
+
+            <Footer />
         </div>
     );
 };
